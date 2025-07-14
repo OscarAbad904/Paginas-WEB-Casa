cb7289467bad084b560e0c8b59b38e745c9833ec
OscarAbad904 <sinnombre904@hotmail.com>
2025-07-14 12:29:02 +0200

    borron y cuenta nueva

--- orbitas sistema Solar/src/ui/HUD.jsx
+++
@@ -1,3 +0,0 @@
-export default function HUD() {
-  return <div className="hud absolute top-0 right-0 p-4">HUD</div>;
-}
--- orbitas sistema Solar/src/ui/Timeline.jsx
+++
@@ -1,3 +0,0 @@
-export default function Timeline() {
-  return <div className="timeline absolute bottom-0 left-0 w-full p-2 bg-black/50 text-white">Timeline</div>;
-}
